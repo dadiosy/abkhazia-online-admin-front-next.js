8ca5697f56a10083cea23a2c8b6721b2d9c00a82
dadiosy <roberto.song940726@gmail.com>
2024-07-22 11:53:40 -0700

    fix bug

--- src/pages/auth/info.jsx
+++ src/pages/auth/info.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 import { useDropzone } from 'react-dropzone';
 import { API_BASE_URL } from '../../const/CustomConsts';
 import { ToastContainer, toast } from 'react-toastify';
-import NavBar from "../components/Layout/NavBar";
+import NavBar from "../components/layout/NavBar";
 
 const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 //const phoneRegex = /^[\+]?[(]?[0-9]{Router3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
